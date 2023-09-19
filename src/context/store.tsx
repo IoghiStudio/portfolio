de83b475b771945dd9f424261dcc45c4854d1ae6
Nicusor Iorga <ioghigithublab@gmail.com>
2023-09-19 10:10:07 +0300

    started file structure

---
+++ src/context/store.tsx
@@ -0,0 +1,36 @@
+"use client";
+import {
+  useContext,
+  createContext,
+  Dispatch,
+  SetStateAction,
+  useState,
+  ReactNode
+} from "react";
+
+interface ContextProps {
+  myName: string;
+  setMyName: Dispatch<SetStateAction<string>>;
+};
+
+const AppContext = createContext<ContextProps>({
+  myName: '',
+  setMyName: () => {}
+});
+
+export const useAppContext = () => useContext(AppContext);
+
+export const AppContextProvider = ({ children } : { children: ReactNode}) => {
+  const [myName, setMyName] = useState<string>('');
+
+  return (
+    <AppContext.Provider 
+      value={{
+        myName,
+        setMyName,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  )
+}
