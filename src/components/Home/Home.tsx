568193b94429656f68a4cc03c9039157af97c0d7
Nicusor Iorga <ioghigithublab@gmail.com>
2023-09-23 22:35:19 +0300

    update

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -9,15 +9,38 @@ export const Home = () => {
         <div className="home__title">
           <TagWrapper tagType={TagType.H1} >
             <div className="home__title-word">
-              Hi,
+              <span className='home__title-letter'>H</span>
+              <span className='home__title-letter'>i</span>
+              <span className='home__title-letter'>,</span>
             </div>
 
             <div className="home__title-word">
-              I'm Nicu,
+              <span className='home__title-letter'>I</span>
+              <span className='home__title-letter'>,</span>
+              <span className='home__title-letter'>m</span>
+              {' '}
+              <span className='home__title-letter'>N</span>
+              <span className='home__title-letter'>i</span>
+              <span className='home__title-letter'>c</span>
+              <span className='home__title-letter'>u</span>
+              <span className='home__title-letter'>,</span>
             </div>
 
             <div className="home__title-word">
-              Web developer
+              <span className='home__title-letter'>W</span>
+              <span className='home__title-letter'>e</span>
+              <span className='home__title-letter'>b</span>
+              {' '}
+              <span className='home__title-letter'>d</span>
+              <span className='home__title-letter'>e</span>
+              <span className='home__title-letter'>v</span>
+              <span className='home__title-letter'>e</span>
+              <span className='home__title-letter'>l</span>
+              <span className='home__title-letter'>o</span>
+              <span className='home__title-letter'>p</span>
+              <span className='home__title-letter'>e</span>
+              <span className='home__title-letter'>r</span>
+              
             </div>
           </TagWrapper>
         </div>
